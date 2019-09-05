6104104186b2f8167e20440635f2bf147df94f8c
halotroop2288 <carolinejoybell@gmail.com>
2019-09-05 13:29:14 -0700

    New logo

--- src/main/java/com/github/craftforever/infinitefeatures/InfiniteFeatures.java
+++ src/main/java/com/github/craftforever/infinitefeatures/InfiniteFeatures.java
@@ -1,15 +1,13 @@
 package com.github.craftforever.infinitefeatures;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -24,7 +22,7 @@ public class InfiniteFeatures
 
 	public InfiniteFeatures()
 	{
-		
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 	
 	@SubscribeEvent
@@ -34,9 +32,6 @@ public class InfiniteFeatures
 		saveDir = world.getSaveHandler().getWorldDirectory();
 		
 		final File generationFIle = new File(saveDir, "infConfig");
-		if (world.getBlockState(new BlockPos(new Vec3d(0D,0D,0D))) == null) // get if (not) world is generated at 0,0,0, or similar effect, which is essentially true whenever the world is generated
-		{
-			
-		}
+		logger.info("File generated here: " + generationFIle.toPath().toString());
 	}
 }
